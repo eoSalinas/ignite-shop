f1707d7bd34868b2b40970d4c6915c1e343ef8a5
Salinas <matheussalinas3@gmail.com>
2022-11-22 16:01:12 -0300

    Estrutural visual - configurando o Stitches

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,17 @@
+import { styled } from '../styles'
+
+const Button = styled('button', {
+  backgroundColor: '$rocketseat',
+  border: 0,
+  borderRadius: 4,
+  padding: '4px 8px',
+  cursor: 'pointer',
+
+  '&:hover': {
+    filter: 'brightness(0.9)',
+  },
+})
+
 export default function Home() {
-  return <h1>Hello World</h1>
+  return <Button>Hello World</Button>
 }
