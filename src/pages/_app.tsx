f20fbc234c310f6e081dbb689b49a26d0569c9b8
Salinas <matheussalinas3@gmail.com>
2022-11-22 16:35:12 -0300

    Estrutura visual - Estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
 import type { AppProps } from 'next/app'
+import { GlobalStyle } from '../styles/global'
+
+GlobalStyle()
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
