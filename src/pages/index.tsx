140a3ebcbde2d68201295184ef9070ffa91ee3c2
Salinas <matheussalinas3@gmail.com>
2022-11-22 22:59:42 -0300

    Estrutura visual - Criando carrossel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,22 @@
+import 'keen-slider/keen-slider.min.css'
+import { useKeenSlider } from 'keen-slider/react'
 import Image from 'next/image'
+import { HomeContainer, Product } from '../styles/pages/home'
 
 import camiseta1 from '../assets/camisetas/shirt1.png'
 import camiseta2 from '../assets/camisetas/shirt2.png'
-import { HomeContainer, Product } from '../styles/pages/home'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 2,
+      spacing: 48,
+    },
+  })
+
   return (
-    <HomeContainer>
-      <Product href="">
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product href="" className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
@@ -16,7 +25,25 @@ export default function Home() {
         </footer>
       </Product>
 
-      <Product href="">
+      <Product href="" className="keen-slider__slide">
+        <Image src={camiseta2} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product href="" className="keen-slider__slide">
+        <Image src={camiseta2} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product href="" className="keen-slider__slide">
         <Image src={camiseta2} alt="" />
 
         <footer>
