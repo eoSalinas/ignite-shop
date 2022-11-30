80ceb57c640cedf1dd39f73798afcf26fa7e3230
Salinas <matheussalinas3@gmail.com>
2022-11-30 11:21:33 -0300

    Header - Cart button estructure

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from 'next/app'
 import Image from 'next/image'
+import Link from 'next/link'
+import { Handbag } from 'phosphor-react'
 import logoImg from '../assets/logo.svg'
 import { GlobalStyle } from '../styles/global'
-import { Container, Header } from '../styles/pages/app'
+import { Cart, Container, Header } from '../styles/pages/app'
 
 GlobalStyle()
 
@@ -10,7 +12,14 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Link href="/" prefetch={false}>
+          <Image src={logoImg} alt="" />
+        </Link>
+
+        <Cart>
+          <span>1</span>
+          <Handbag weight="bold" size={24} />
+        </Cart>
       </Header>
       <Component {...pageProps} />
     </Container>
--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -13,4 +13,37 @@ export const Header = styled('header', {
   width: '100%',
   maxWidth: 1180,
   margin: '0 auto',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+})
+
+export const Cart = styled('button', {
+  lineHeight: '0', // Make button height the same as icon
+  padding: '0.75rem',
+  border: 0,
+  borderRadius: 6,
+  backgroundColor: '$gray800',
+  color: '$gray500',
+
+  '&:hover': {
+    opacity: 0.8,
+    cursor: 'pointer',
+  },
+
+  span: {
+    position: 'absolute',
+    width: 24,
+    height: 24,
+    backgroundColor: '$green500',
+    borderRadius: 9999,
+    marginTop: 'calc(-27px - -7px)',
+    marginLeft: 'calc(27px - 7px)', // (heigh + outline) - difference from radio(7px)
+    color: 'white',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    outline: '3px solid $gray900',
+    fontWeight: 'bold',
+  },
 })
